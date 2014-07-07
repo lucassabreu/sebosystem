d17b7d62971f9a7e79cdde1e82c45ea22159a8f4
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2014-07-06 23:46:19 -0300

    Tentativa Spring / Adição Maven POM.xml / Tela de Autenticação

--- SeboSystemEJB/ejbModule/com/sebosystem/dao/User.java
+++ SeboSystemEJB/ejbModule/com/sebosystem/dao/User.java
@@ -12,129 +12,129 @@ import javax.persistence.Id;
 
 @Entity
 public class User implements Serializable {
-	private static final long serialVersionUID = 3800255543775713159L;
+    private static final long serialVersionUID = 3800255543775713159L;
 
-	@Id
-	@Column(updatable = false)
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long oid;
+    @Id
+    @Column(updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long oid;
 
-	@Column(nullable = false, length = 100)
-	private String name;
+    @Column(nullable = false, length = 100)
+    private String name;
 
-	@Column(nullable = false, unique = true, length = 60)
-	private String email;
+    @Column(nullable = false, unique = true, length = 60)
+    private String email;
 
-	@Column(nullable = false, length = 32)
-	private String password;
+    @Column(nullable = false, length = 32)
+    private String password;
 
-	@Column(nullable = false)
-	private int sumRating;
+    @Column(nullable = false)
+    private int sumRating;
 
-	@Column(nullable = false)
-	private int reviews;
+    @Column(nullable = false)
+    private int reviews;
 
-	@Column(length = 1, nullable = false)
-	@Enumerated(EnumType.ORDINAL)
-	private RoleType role;
+    @Column(length = 1, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
 
-	public User() {
-	}
+    public User() {
+    }
 
-	public User(String name, String email, String password, int sumRating,
-			int reviews, RoleType role) {
-		super();
-		this.name = name;
-		this.email = email;
-		this.password = password;
-		this.sumRating = sumRating;
-		this.reviews = reviews;
-		this.role = role;
-	}
+    public User(String name, String email, String password, int sumRating,
+            int reviews, RoleType role) {
+        super();
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.sumRating = sumRating;
+        this.reviews = reviews;
+        this.role = role;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (oid ^ (oid >>> 32));
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (oid ^ (oid >>> 32));
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (oid != other.oid)
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (oid != other.oid)
+            return false;
+        return true;
+    }
 
-	public int getRating() {
-		if (this.reviews > 0)
-			return this.sumRating / this.reviews;
-		else
-			return 3;
-	}
+    public int getRating() {
+        if (this.reviews > 0)
+            return this.sumRating / this.reviews;
+        else
+            return 3;
+    }
 
-	public long getOid() {
-		return oid;
-	}
+    public long getOid() {
+        return oid;
+    }
 
-	public void setOid(long oid) {
-		this.oid = oid;
-	}
+    public void setOid(long oid) {
+        this.oid = oid;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public int getSumRating() {
-		return sumRating;
-	}
+    public int getSumRating() {
+        return sumRating;
+    }
 
-	public void setSumRating(int sumRating) {
-		this.sumRating = sumRating;
-	}
+    public void setSumRating(int sumRating) {
+        this.sumRating = sumRating;
+    }
 
-	public int getReviews() {
-		return reviews;
-	}
+    public int getReviews() {
+        return reviews;
+    }
 
-	public void setReviews(int reviews) {
-		this.reviews = reviews;
-	}
+    public void setReviews(int reviews) {
+        this.reviews = reviews;
+    }
 
-	public RoleType getRole() {
-		return role;
-	}
+    public RoleType getRole() {
+        return role;
+    }
 
-	public void setRole(RoleType role) {
-		this.role = role;
-	}
+    public void setRole(RoleType role) {
+        this.role = role;
+    }
 
 }
